05bfed25cdf69627ead3e4f21fc9088254a940ee
Ryota NIINOMI Takemoto <r21nomi@gmail.com>
2023-07-27 17:39:24 +0900

    Redirect about to cv

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,7 @@ import Header from "~/components/Header.vue";
 import Footer from "~/components/Footer.vue";
 
 @Component({
+    middleware: ["redirect"],
     components: { Header, Footer }
 })
 export default class extends Vue {
